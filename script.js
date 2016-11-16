45f1ebec291b80aa17848d940502b522ca5f17ce
danielsaffar <danielsaffar@gmail.com>
2016-11-16 17:15:24 +0200

    cart

--- script.js
+++ script.js
@@ -2,8 +2,17 @@
 var cart = [];
 
 var updateCart = function () {
-  // TODO: finish
-}
+
+var source = $('#store-template').html();
+var template = Handlebars.compile(source);
+
+
+for (i=0; i<cart.length;i++) {
+    var newHTML = template(cart[i]);
+ $('.shopping-cart').append(newHTML);
+  
+}}
+
 
 
 
@@ -18,6 +27,7 @@ $('.view-cart').on('click', function () {
 	
   // TODO: hide/show the shopping cart!
 });
+
 $('.add-to-cart').on('click', function () {
  console.log("LEt's add Stuff!!")
  var $item = $(this).closest(".item"); 
@@ -33,23 +43,13 @@ var addItem = function (item) {
   // TODO: finish
   cart.push(item);
   console.log("item should be pushed by now");
-  debugger;
-}
+};
 
 
-// var source = $('#store-template').html();
-// var template = Handlebars.compile(source);
-
-
-// for (i=0; i<cart.shopcart.length;i++) {
-//     var newHTML = template(items.item1[i]);
-//  $('.item1').append(newHTML);
-  
-// };
 
 // $('.clear-cart').on('click', function () {
 //   clearCart();
 // });
 
 // update the cart as soon as the page loads!
-updateCart();
+// updateCart();
